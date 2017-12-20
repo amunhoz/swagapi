72a74d52693084c1070a102a8cdeac446a19d5c2
munhoz@paraleloz.in <munhoz@paraleloz.in>
2017-12-20 17:07:45 -0200

    implementado body-limit

--- middleware/express-basic.js
+++ middleware/express-basic.js
@@ -3,29 +3,31 @@ module.exports = {
   name: "express-basic",
 	run: async function (appExpress) {
 	  
-    var bodyParser = require('body-parser')
-		appExpress.use(bodyParser.urlencoded({ extended: true }))
-		appExpress.use(bodyParser.json())
+		var bodyParser = require('body-parser')
+		var limit = "50mb"
+		if (app.config['body-limit']) limit = app.config['body-limit']
+		appExpress.use(bodyParser.urlencoded({limit: limit, extended: true }))
+		appExpress.use(bodyParser.json({limit: limit}))
 	
-	var compression = require('compression');
-	appExpress.use(compression())
-	
-	var cookieParser = require('cookie-parser');
-	appExpress.use(cookieParser());
-	
-	var cookieSession = require('cookie-session');
-	appExpress.use(cookieSession({
-		name: 'session',
-		keys: ['lalala', 'bububububububub']
-	}));
-	
-	var session = require('express-session');
-	appExpress.use(session({
-	  secret: 'keyboard categorie lalala',
-	  resave: false,
-	  saveUninitialized: true,
-	  cookie: { secure: true }
-	}))
+		var compression = require('compression');
+		appExpress.use(compression())
+		
+		var cookieParser = require('cookie-parser');
+		appExpress.use(cookieParser());
+		
+		var cookieSession = require('cookie-session');
+		appExpress.use(cookieSession({
+			name: 'session',
+			keys: ['lalala', 'bububububububub']
+		}));
+		
+		var session = require('express-session');
+		appExpress.use(session({
+			secret: 'keyboard categorie lalala',
+			resave: false,
+			saveUninitialized: true,
+			cookie: { secure: true }
+		}))
 
 
   }
